25c8bed66569711607f06811e5829d45f5449f78
Lina Aldadah <linaaldadah@Linas-MacBook-Air-2.local>
2025-11-14 15:16:16 -0600

    changed project title

--- GeometryExhibition/HomeView.swift
+++ GeometryExhibition/HomeView.swift
@@ -37,7 +37,7 @@ struct HomeView: View {
                 }
             }
             .padding()
-            .navigationTitle("Geometry Exhibition Demo")
+            .navigationTitle("Spades")
             .navigationBarTitleDisplayMode(.inline)
         }
     }
